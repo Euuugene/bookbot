7907acd74dac604b5de42c013e5da796a0f8dcab
Euuugene <jos2002130@gmail.com>
2024-10-28 00:10:09 -0700

    Add count_characters function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
-    get_book_text("books/frankenstein.txt")
-    count_words("books/frankenstein.txt")
+    # get_book_text("books/frankenstein.txt")
+    # count_words("books/frankenstein.txt")
+    charc_dict = count_characters("books/frankenstein.txt")
+    print(charc_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,4 +13,17 @@ def count_words(path):
             content = f.read().split()
         print(len(content))
 
+def count_characters(path):
+    characters_set = {chr(i+96):0 for i in range(1,27)}
+
+    with open(path) as f:
+        content = f.read()
+
+    for c in content:
+        if c.isalpha():
+            characters_set[c.lower()] += 1
+
+    return characters_set
+
+                
 main()
